460a98b7cbe8590ae29d9360d6aad8ac8d68cab9
oneaxissolutions <oneaxissolutions@email.com>
2025-12-17 18:20:38 +0530

    Corrected redirects file and updated config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-            tailwindcss() 
+  plugins: [
+    react(),
+    tailwindcss()
   ],
+  // Base path ko './' karne se Netlify ko files dhundne mein aasani hoti hai
+  base: './', 
+  build: {
+    outDir: 'dist',
+  }
 })
